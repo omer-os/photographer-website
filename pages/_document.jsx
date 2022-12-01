c0c1bc3cce70d83c2c848e416904d9cf31aec215
omar chatin <omerchatin19@gmail.com>
2022-12-02 02:13:22 +0300

    f

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -14,32 +14,24 @@ export default class MyDocument extends Document {
             name="keywords"
             content="photographer, videographer, photoshop, montaj"
           />
-          <meta name="robots" content="index, follow" />
-          <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-          <meta name="language" content="English" />
-          <meta name="author" content="morris" />
-          <meta property="og:url" content="http://www.sadiqghazi.com/" />
-          <meta property="og:site_name" content="Sadiq Ghazi" />
-          <meta property="og:title" content="Sadiq Ghaz" />
-          <meta property="og:description" content="welcome to sadiq ghazi's official portfolio website. im profissional photographer and videographer based on iraq." />
-          <meta property="og:image" content="https://www.sadiqghazi.com/_next/image?url=https%3A%2F%2Fmedia.graphassets.com%2FQVXxbLl3SzuKzX0vgzD3&w=1920&q=75" />
-          <meta property="og:image:type" content="image/png" />
-          <meta property="og:image:width" content="1200" />
-          <meta property="og:image:height" content="630" />
-          <meta property="og:image:alt" content="Sadiq Ghaz" />
-          <meta property="og:image" content="https://www.sadiqghazi.com/_next/image?url=https%3A%2F%2Fmedia.graphassets.com%2FQVXxbLl3SzuKzX0vgzD3&w=1920&q=75" />
-          <meta property="og:image:type" content="image/png" />
-          <meta property="og:image:width" content="600" />
-          <meta property="og:image:height" content="314" />
-          <meta property="og:image:alt" content="Sadiq Ghaz" />
-          <meta property="og:image" content="https://www.sadiqghazi.com/_next/image?url=https%3A%2F%2Fmedia.graphassets.com%2FQVXxbLl3SzuKzX0vgzD3&w=1920&q=75" />
-          <meta property="og:image:type" content="image/png" />
-          <meta property="og:image:width" content="400" />
-          <meta property="og:image:height" content="400" />
-          <meta property="og:image:alt" content="Sadiq Ghaz" />
-          <meta name="twitter:card" content="summary" />
-          <meta name="twitter:site" content="#site_twitter" />
-          <meta name="twitter:creator" content="#creator_twitter" />
+
+          <meta property="og:title" content="Sadiq Ghazi" />
+          <meta property="og:url" content="https://www.sadiqghazi.com/" />
+          <meta
+            property="og:image"
+            content="https://www.sadiqghazi.com/_next/image?url=https%3A%2F%2Fmedia.graphassets.com%2FQVXxbLl3SzuKzX0vgzD3&w=1920&q=75"
+          />
+
+          <meta name="twitter:title" content="Sadiq Ghazi " />
+          <meta
+            name="twitter:description"
+            content=" welcome to sadiq ghazi's official portfolio website. im profissional photographer and videographer based on iraq."
+          />
+          <meta
+            name="twitter:image"
+            content="https://www.sadiqghazi.com/_next/image?url=https%3A%2F%2Fmedia.graphassets.com%2FQVXxbLl3SzuKzX0vgzD3&w=1920&q=75"
+          />
+          <meta name="twitter:card" content="https://www.sadiqghazi.com/_next/image?url=https%3A%2F%2Fmedia.graphassets.com%2FQVXxbLl3SzuKzX0vgzD3&w=1920&q=75" />
         </Head>
         <body>
           <Main />
